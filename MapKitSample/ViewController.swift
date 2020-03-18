b725288bf229bf0f54bdf438642c169bb509fa5f
Take111 <maxmaxastea@gmail.com>
2020-03-18 23:34:06 +0900

    feat:

--- MapKitSample/ViewController.swift
+++ MapKitSample/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        
+        
     }
 
+    override func viewWillLayoutSubviews() {
+        
+        
+    }
+    
+    
 
 }
 
