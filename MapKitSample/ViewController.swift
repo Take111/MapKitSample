59b6a1d4ba9dba742cd779e57500b884aea88dd9
Take111 <maxmaxastea@gmail.com>
2020-03-18 22:57:04 +0900

    Initial Commit

---
+++ MapKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitSample
+//
+//  Created by 竹ノ内愛斗 on 2020/03/18.
+//  Copyright © 2020 竹ノ内愛斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
